7294b912c7729349d80c14156fde7e9283f7fb34
Juan Sebastian Usuga Agudelo <usuga1997@gmail.com>
2025-01-19 09:35:20 -0500

    I created the folder structure and installed React Navigation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigation } from './presentation/navigation/Navigation';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+  );
+};
