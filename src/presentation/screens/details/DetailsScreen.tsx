4faa7e897eb75dc4d7a9febe957b6dee2a11f5ab
Juan Sebastian Usuga Agudelo <usuga1997@gmail.com>
2025-01-20 20:14:26 -0500

    I created a carousel to show the popular movies

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export const DetailsScreen = () => {
   return (
     <View>
-      <Text>Hello fucking world!!!</Text>
+      <Text>Hello world!!!</Text>
     </View>
   );
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -5,10 +5,11 @@ import { useMovies } from '../../hooks/useMovies';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { PosterCarousel } from '../../components/movies/PosterCarousel';
+import { HorizontalCarousel } from '../../components/movies/HorizontalCarousel';
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
-  const { isLoading, nowPlaying } = useMovies();
+  const { isLoading, nowPlaying, popular, topRated, upcoming } = useMovies();
 
   if (isLoading) {
     return (<Text>Loading...</Text>);
@@ -20,7 +21,17 @@ export const HomeScreen = () => {
         marginTop: top + 20,
         paddingBottom: 30,
       }}>
+        {/* Movies now playing */}
         <PosterCarousel movies={nowPlaying} />
+
+        {/* Popular movies */}
+        <HorizontalCarousel movies={popular} title="Populares" />
+
+        {/* TopRated movies */}
+        <HorizontalCarousel movies={topRated} title="Mejor calificadas" />
+
+        {/* UpComnig movies */}
+        <HorizontalCarousel movies={upcoming} title="Próximamente" />
       </View>
     </ScrollView>
   );
